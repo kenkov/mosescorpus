c8aea15cc24d845ebd53dcc3adc9a8e0211e0151
Noriyuki Abe <kenko.py@gmail.com>
2013-05-09 21:10:34 -0700

    Remove post_format.py and pre_format.py and create format.py instead
    - merge post_format.py and pre_format.py into format.py
    - revise Makefile

--- scripts/archwiki/format.py
+++ scripts/archwiki/format.py
@@ -6,13 +6,26 @@ import re
 
 
 def substitute(text):
-    re_strs = [ur'^\s+$',
-               ur'\s+$',
-               ur'^\s+',
+    re_strs = [ur'^\s*\d(\.\d)+\.?\s*',
+               ur'note:\s*',
+               ur'(日本語)',
+               ur'(http|https)://[a-zA-Z0-9-./"#$%&\':?=_]+',
                ]
     for re_str in re_strs:
         re_obj = re.compile(re_str)
         text = re_obj.sub("", text)
+    return text
+
+
+def split(text):
+    re_strs = [ur'(:)\s*\S+.*$',
+               ur'(;)\s*\S+.*$',
+               ur'(\.)\s*\S+.*$',
+               ur'(。)\s*\S+.*$',
+               ]
+    for re_str in re_strs:
+        re_obj = re.compile(re_str)
+        text = re_obj.sub(u"\\1\n", text)
     return text
 
 
@@ -29,17 +42,17 @@ def is_ignore_line(text):
 
 if __name__ == '__main__':
     import sys
+    import codecs
     try:
         input_file = sys.argv[1]
-        sys.stdin = open(input_file, "r")
+        sys.stdin = codecs.open(input_file, "r", 'utf-8')
     except IndexError:
         pass
 
     with sys.stdin as f:
-        for line in f.readlines():
-            text = substitute(line)
-            text = line.strip()
+        for line in (line.strip() for line in f):
+            text = split(substitute(line))
             if is_ignore_line(text):
                 continue
             else:
-                print text
+                print text.strip().encode('utf-8')
